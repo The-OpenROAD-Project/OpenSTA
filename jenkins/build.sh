905626f607ceb55f70b14eae6ebf315ab2dd2c3c
James Cherry <cherry@parallaxsw.com>
2020-01-27 12:01:05 -0700

    jenkins

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
 set -e
-docker build -t openroad/openroad --target base-dependencies .
-docker run -u $(id -u ${USER}):$(id -g ${USER}) -v $(pwd):/OpenSTA openroad/openroad bash -c "./OpenSTA/jenkins/install.sh"
+docker build -t openroad/opensta --target base-dependencies .
+docker run -u $(id -u ${USER}):$(id -g ${USER}) -v $(pwd):/OpenSTA openroad/opensta bash -c "./OpenSTA/jenkins/install.sh"
