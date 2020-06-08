88c2c61ed768589abea25e6f73c8926e991011d0
James Cherry <cherry@parallaxsw.com>
2020-06-08 16:21:51 -0700

    report min_period Period -> period

--- search/CheckFanoutLimits.cc
+++ search/CheckFanoutLimits.cc
@@ -83,8 +83,6 @@ CheckFanoutLimits::init(const MinMax *min_max)
 			   top_limit, top_limit_exists);
   top_limit_= top_limit;
   top_limit_exists_ = top_limit_exists;
-  if (top_limit_exists_)
-    printf("luse\n");
 }
 
 void
