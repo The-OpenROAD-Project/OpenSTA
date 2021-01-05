e4159bd6519d205b17961a5765d4805bc6e386b3
James Cherry <cherry@parallaxsw.com>
2021-01-04 18:16:03 -0800

    gcc compiler warnings

--- search/TagGroup.cc
+++ search/TagGroup.cc
@@ -326,14 +326,15 @@ TagGroupBldr::copyArrivals(TagGroup *tag_group,
     arrival_iter1.next(tag1, arrival_index1);
     bool exists2;
     tag_group->arrivalIndex(tag1, arrival_index2, exists2);
-    if (exists2)
+    if (exists2) {
       arrivals[arrival_index2] = arrivals_[arrival_index1];
+      if (prev_paths) {
+	PathVertexRep *prev_path = &prev_paths_[arrival_index1];
+	prev_paths[arrival_index2].init(prev_path);
+      }
+    }
     else
       sta_->report()->critical(265, "tag group missing tag");
-    if (prev_paths) {
-      PathVertexRep *prev_path = &prev_paths_[arrival_index1];
-      prev_paths[arrival_index2].init(prev_path);
-    }
   }
 }
 
