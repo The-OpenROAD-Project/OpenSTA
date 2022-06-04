15b1084e9683804c0cd4baf1f527383120762daf
James Cherry <cherry@parallaxsw.com>
2022-06-04 08:16:36 -0700

    copyright
    Signed-off-by: James Cherry <cherry@parallaxsw.com>

--- util/MachineWin32.cc
+++ util/MachineWin32.cc
@@ -1,10 +1,18 @@
-// Parallax Static Timing Analyzer
-// Copyright (c) 2021, Parallax Software, Inc.
-// All rights reserved.
+// OpenSTA, Static Timing Analyzer
+// Copyright (c) 2022, Parallax Software, Inc.
 // 
-// No part of this document may be copied, transmitted or
-// disclosed in any form or fashion without the express
-// written consent of Parallax Software, Inc.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 #include "Machine.hh"
 
