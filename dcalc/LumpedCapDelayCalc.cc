b819c4d429b1731220bf7e755632451eeea171fd
James Cherry <cherry@parallaxsw.com>
2022-09-22 11:45:10 -0700

    gateDelay error on NaN dcalc var
    Signed-off-by: James Cherry <cherry@parallaxsw.com>

--- dcalc/LumpedCapDelayCalc.cc
+++ dcalc/LumpedCapDelayCalc.cc
@@ -163,6 +163,9 @@ LumpedCapDelayCalc::gateDelay(const LibertyCell *drvr_cell,
     ArcDelay gate_delay1;
     Slew drvr_slew1;
     float in_slew1 = delayAsFloat(in_slew);
+    // NaNs cause seg faults during table lookup.
+    if (isnan(load_cap) || isnan(related_out_cap) || isnan(in_slew))
+      report_->error(705, "gate delay input variable is NaN");
     model->gateDelay(drvr_cell, pvt, in_slew1, load_cap, related_out_cap,
 		     pocv_enabled_, gate_delay1, drvr_slew1);
     gate_delay = gate_delay1;
