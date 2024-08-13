93005762b3e2ee457f275aeb87121faa9e792163
Akash Levy <akashlevy@gmail.com>
2024-08-12 20:50:21 -0700

    Use asap7_small lib for speed

--- test/get_filter.tcl
+++ test/get_filter.tcl
@@ -1,7 +1,5 @@
 # Read in design and libraries
-read_liberty asap7_invbuf.lib.gz
-read_liberty asap7_seq.lib.gz
-read_liberty asap7_simple.lib.gz
+read_liberty asap7_small.lib.gz
 read_verilog reg1_asap7.v
 link_design top
 create_clock -name clk -period 500 {clk1 clk2 clk3}
@@ -17,13 +15,13 @@ report_object_full_names [get_clocks -filter is_virtual==1 *]
 puts "get_lib_cells"
 report_object_full_names [get_lib_cells -filter is_buffer==1 *]
 puts "get_lib_cells 2"
-report_object_full_names [get_lib_cells -filter is_inverter==1 *]
+report_object_full_names [get_lib_cells -filter is_inverter==0 *]
 puts "get_lib_pins"
 report_object_full_names [get_lib_pins -filter direction==input BUFx2_ASAP7_75t_R/*]
 puts "get_lib_pins 2"
 report_object_full_names [get_lib_pins -filter direction==output BUFx2_ASAP7_75t_R/*]
 puts "get_libs"
-report_object_full_names [get_libs -filter name==asap7sc7p5t_INVBUF_RVT_TT_ccs_211120 *]
+report_object_full_names [get_libs -filter name==asap7_small *]
 puts "get_nets"
 report_object_full_names [get_nets -filter name=~*q *]
 puts "get_pins"
