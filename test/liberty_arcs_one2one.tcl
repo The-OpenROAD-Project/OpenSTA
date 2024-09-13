9c7cfb4efb8b473ffc76264474052704d715e9d7
Akash Levy <akashlevy@gmail.com>
2024-09-12 18:15:17 -0700

    Use report_edges instead

--- test/liberty_arcs_one2one.tcl
+++ test/liberty_arcs_one2one.tcl
@@ -6,7 +6,10 @@ link_design liberty_arcs_one2one_1
 create_clock -name clk -period 0
 set_input_delay -clock clk 0 [all_inputs]
 set_output_delay -clock clk 0 [all_outputs]
-report_checks -format summary -group_count 5
+for {set i 0} {$i < 8} {incr i} {
+    puts "report_edges -from partial_wide_inv_cell/A[$i]"
+    report_edges -from partial_wide_inv_cell/A[$i]
+}
 
 puts "TEST 2:"
 read_verilog liberty_arcs_one2one_2.v
@@ -14,4 +17,7 @@ link_design liberty_arcs_one2one_2
 create_clock -name clk -period 0
 set_input_delay -clock clk 0 [all_inputs]
 set_output_delay -clock clk 0 [all_outputs]
-report_checks -format summary -group_count 5
+for {set i 0} {$i < 8} {incr i} {
+    puts "report_edges -to partial_wide_inv_cell/Y[$i]"
+    report_edges -to partial_wide_inv_cell/Y[$i]
+}
