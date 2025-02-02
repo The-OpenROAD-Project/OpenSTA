The-OpenROAD-Project/OpenSTA

commit dbd95cce7d00866172d9a92b190e7eac5b8c3042

source set info script
Signed-off-by: James Cherry <cherry@parallaxsw.com>

sdc/Sdc.tcl
@@ -42,7 +42,13 @@ proc_redirect read_sdc {
   check_argc_eq1 "read_sdc" $args
   set echo [info exists flags(-echo)]
   set filename [file nativename [lindex $args 0]]
-  source_ $filename $echo 0
+  set prev_filename [info script]
+  try {
+    info script $filename
+    source_ $filename $echo $verbose
+  } finally {
+    info script $prev_filename
+  }
 }
 
 ################################################################
@@ -70,7 +76,13 @@ proc_redirect source {
   set echo [info exists flags(-echo)]
   set verbose [info exists flags(-verbose)]
   set filename [file nativename [lindex $args 0]]
-  source_ $filename $echo $verbose
+  set prev_filename [info script]
+  try {
+    info script $filename
+    source_ $filename $echo $verbose
+  } finally {
+    info script $prev_filename
+  }
 }
 
 proc source_ { filename echo verbose } {

commit 988514f8f4300582119d326288e6ae1d545d8bf6

read_sdc bug introduced by dbd95cce
Signed-off-by: James Cherry <cherry@parallaxsw.com>

sdc/Sdc.tcl
@@ -45,7 +45,7 @@ proc_redirect read_sdc {
   set prev_filename [info script]
   try {
     info script $filename
-    source_ $filename $echo $verbose
+    source_ $filename $echo 0
   } finally {
     info script $prev_filename
   }