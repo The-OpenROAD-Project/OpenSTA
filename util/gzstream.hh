335b305149900901fda29c02405429c790053de6
Florian Chivé <62927863+Faholan@users.noreply.github.com>
2025-05-29 13:59:40 -0700

    Change memcpy to memmove
    Signed-off-by: Florian Chivé <62927863+Faholan@users.noreply.github.com>

--- util/gzstream.hh
+++ util/gzstream.hh
@@ -117,7 +117,7 @@ public:
         int n_putback = gptr() - eback();
         if ( n_putback > 4)
             n_putback = 4;
-        memcpy( buffer + (4 - n_putback), gptr() - n_putback, n_putback);
+        memmove( buffer + (4 - n_putback), gptr() - n_putback, n_putback);
         
         int num = gzread( file, buffer+4, bufferSize-4);
         if (num <= 0) // ERROR or EOF
