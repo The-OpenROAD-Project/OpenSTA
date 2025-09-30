b4565890071400397269d72c58a1ac8d42a835c3
James Cherry <cherry@parallaxsw.com>
2025-09-29 19:45:04 -0700

    PropActivityVisitor::visit null port check PR 301
    Signed-off-by: James Cherry <cherry@parallaxsw.com>

--- power/Power.cc
+++ power/Power.cc
@@ -490,10 +490,11 @@ PropActivityVisitor::visit(Vertex *vertex)
     }
     if (network_->isDriver(pin)) {
       LibertyPort *port = network_->libertyPort(pin);
-      LibertyCell *test_cell = port->libertyCell()->testCell();
-      if (test_cell)
-	port = test_cell->findLibertyPort(port->name());
       if (port) {
+        LibertyCell *test_cell = port->libertyCell()->testCell();
+        if (test_cell)
+          port = test_cell->findLibertyPort(port->name());
+
 	FuncExpr *func = port->function();
 	if (func) {
           PwrActivity activity = power_->evalActivity(func, inst);
