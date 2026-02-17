f37cac01c4600c804038a241a8b63aeab256fd08
dsengupta0628 <dsengupta@precisioninno.com>
2026-02-17 20:03:04 +0000

    Remove redundant space from testacse, but kept ones to separate testing blocks
    Signed-off-by: dsengupta0628 <dsengupta@precisioninno.com>

--- test/report_checks_default_path_grp.tcl
+++ test/report_checks_default_path_grp.tcl
@@ -1,11 +1,10 @@
-
 # report_checks all fields enabled
 read_liberty ../examples/sky130hd_tt.lib.gz
 read_verilog verilog_attribute.v
 link_design counter
+
 create_clock -name clk -period 10 clk
 set_input_delay -clock clk 0 [all_inputs -no_clocks]
-
 report_checks -path_group clk -fields {capacitance slew input_pin net src_attr}
 
 set module_name "_141"
@@ -18,11 +17,7 @@ set x [sta::path_group_names]
 puts $x
 report_checks -path_group "crash_group"
 
-
 group_path -from $all_module_cells -to $all_module_cells -default
 set x [sta::path_group_names]
 puts $x
 report_checks -path_group "crash_group"
-
-
-
