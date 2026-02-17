1c1bef30d54727b253750719c3370a135c04be19
dsengupta0628 <dsengupta@precisioninno.com>
2026-02-17 20:19:17 +0000

    test functionality with dollar sign, also need regoldening an openroad regression with another commit
    Signed-off-by: dsengupta0628 <dsengupta@precisioninno.com>

--- test/test_write_verilog_escape.v
+++ test/test_write_verilog_escape.v
@@ -9,6 +9,7 @@ module \hier_block (childclk, \Y[2:1] );
    input childclk; 
    output [1:0] \Y[2:1] ;
    wire [1:0] \Y[2:1] ;
+   BUFx2_ASAP7_75t_R \abuf_$100  (.A(childclk));
    BUFx2_ASAP7_75t_R \ff0/name (.A(childclk));
 endmodule // hier_block1
 
