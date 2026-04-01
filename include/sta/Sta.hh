211aea74971c89a319a22f816e33d83eb86a67b9
Cho Moon <cmoon@precisioninno.com>
2026-04-01 00:12:26 +0000

    added one more Sta API
    Signed-off-by: Cho Moon <cmoon@precisioninno.com>

--- include/sta/Sta.hh
+++ include/sta/Sta.hh
@@ -1325,6 +1325,7 @@ public:
   // Ensure that the timing graph has been built.
   Graph *ensureGraph();
   void ensureClkArrivals();
+  Levelize* levelize() const { return levelize_; }
 
   // Find all arc delays and vertex slews with delay calculator.
   virtual void findDelays();
