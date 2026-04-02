d47cbeb76bf6cfbb9ab4f63d53294e1914de84ef
dsengupta0628 <dsengupta@precisioninno.com>
2026-04-02 00:56:38 +0000

    fix to exclude bias pins from timing graph
    Signed-off-by: dsengupta0628 <dsengupta@precisioninno.com>

---
+++ verilog/test/verilog_bias_pins.v
@@ -0,0 +1,20 @@
+module top (
+    output y,
+    input  a
+);
+
+  supply1 VPWR;
+  supply0 VGND;
+  supply1 VPB;
+  supply0 VNB;
+
+  sky130_fd_sc_hd__buf_1 u1 (
+      .X(y),
+      .A(a),
+      .VPWR(VPWR),
+      .VGND(VGND),
+      .VPB(VPB),
+      .VNB(VNB)
+  );
+
+endmodule
