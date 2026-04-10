097daeb3c5b8461b15007d3f3610d19f187bab51
dsengupta0628 <dsengupta@precisioninno.com>
2026-04-10 18:59:42 +0000

    address comment related to Coverity to simplify
    Signed-off-by: dsengupta0628 <dsengupta@precisioninno.com>

--- liberty/Liberty.cc
+++ liberty/Liberty.cc
@@ -2478,10 +2478,8 @@ bool
 LibertyPort::less(const LibertyPort *port1,
                   const LibertyPort *port2)
 {
-  if (port1 == nullptr && port2 != nullptr)
-    return true;
   if (port1 == nullptr || port2 == nullptr)
-    return false;
+    return port1 == nullptr && port2 != nullptr;
   const std::string &name1 = port1->name();
   const std::string &name2 = port2->name();
   if (name1 == name2) {
